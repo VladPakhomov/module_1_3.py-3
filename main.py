f5d7418539ee890fb0d05e9d0b90e4f3b9b43fc6
VladPakhomov <mongebello@gmail.com>
2024-12-18 01:14:50 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+name = 'Vladislav'
+print(type(name))
+age = 52
+print(type(age))
+new_age = age+1
+print(type(new_age))
+print(new_age)
+is_student = True
+print(type(is_student))
+print(is_student)
